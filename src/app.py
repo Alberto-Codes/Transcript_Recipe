eff2897547f87b0a9647c7580f6ceb5a1aa2c562
Alberto Nieto <94092485+Alberto-Codes@users.noreply.github.com>
2023-12-10 14:07:13 +0000

    Add basic web app with Streamlit

---
+++ src/app.py
@@ -0,0 +1,11 @@
+# Import Streamlit
+import streamlit as st
+
+# Create a basic web app with Streamlit
+def main():
+    # Display "Hello World" on the web app
+    st.write("Hello World")
+
+# Run the app
+if __name__ == "__main__":
+    main()
